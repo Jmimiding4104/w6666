0c36b6974c1be3b29a5092c0f8d3f05b4f0dea28
Jmimiding4104@gmail.com <Jmimiding4104>
2023-07-13 03:36:45 +0800

    713

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 import './assets/scss/all.scss';
 
-console.log("Hello world!");
+const hamberger = document.querySelector('.header-hamberger-btn').addEventListener(
+    "click",
+    function(e){
+        e.preventDefault();
+        const headerHambergerList = document.querySelector('.header-hamberger-list').style.maxHeight;
+        if(headerHambergerList == '141px'){
+            document.querySelector('.header-hamberger-list').style.maxHeight = "0px";
+            console.log(headerHambergerList)
+        }else{
+            document.querySelector('.header-hamberger-list').style.maxHeight = "141px";
+            console.log(headerHambergerList)
+        }
+        
+    }
+)
